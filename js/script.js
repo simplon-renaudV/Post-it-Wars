68302a0493a9cc21b9724dc3e0dbe18f241119a0
simplon-renaudV <renaud.vivancos@free.fr>
2016-04-25 12:16:07 +0200

    ajout ajax + couleur aléatoire

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-var couleurs=['rgb(255, 0, 0)', 'rgb(0, 255, 0)', 'rgb(0, 0, 255)', 'rgb(0, 0, 0)'];
+var couleurs=['rgb(255, 0, 0)', 'rgb(0, 255, 0)', 'rgb(0, 0, 255)', 'rgb(0, 0, 0)', 'rgb(255, 255, 255)'];
 var numcase=0;
 var couleur='';
 
@@ -23,20 +23,66 @@ for (var i=0; i<16; i++) {
 	for(var j=0; j<16; j++) {
 		$("body").append("<div class='case case"+numcase+"'></div>");
 		numcase++;
-		$(".case").droppable({
-			addClasses: false,
-			drop: function(event, ui) {
-				var droppe = event.target;
-				droppe.style.backgroundColor = couleur;
-				numero = droppe.className.substr(9);
-				
-				$.ajax({
-					url: 'http://10.200.0.156/js/tutos/jqueryExos/draw.php',
-					type: 'POST',
-					data: 'numcase='+numero+'&couleur='+couleur
-				});
-			}		
-		});
 	}
 	$("body").append("<div class='clear'></div>");
 }
+
+$(".case").droppable({
+	addClasses: false,
+	drop: function(event, ui) {
+		var droppe = event.target;
+		droppe.style.backgroundColor = couleur;
+		numero = droppe.className.substr(9);
+				
+		$.ajax({
+			url: 'http://10.200.0.156/js/tutos/jqueryExos/draw.php',
+			type: 'POST',
+			data: {numcase: numero, couleur: couleur}
+		});
+	}		
+});
+
+$("body").append("<button id='effacer'>Effacer</button>");
+$("body").append("<button id='alea'>Aléatoire</button");
+
+// Effacer toutes les cases de la grille
+$("#effacer").click(function(){
+	for (var i=0; i<256; i++) {
+	$.ajax({
+			url : 'http://10.200.0.156/js/tutos/jqueryExos/draw.php',
+			type: 'POST',
+			data: {numcase: i, couleur: 'rgb(255, 255, 255)'}
+		});
+		$(".case"+i).css("background-color", 'rgb(255, 255, 255');
+	}
+});
+
+// Rafraichissement de la grille distante toutes les 5sec
+setInterval(function(){
+	$.ajax({
+		url: 'http://10.200.0.156/js/tutos/jqueryExos/drawing.json',
+		datatype: 'JSON',
+		data: 'data',
+		success: function(fichJSON) {
+			$.each(fichJSON, function (index, element) {
+				$(".case"+index).css("background-color", element);
+			})
+		}
+	});
+	}, 5000);
+
+// Création d'une case aléatoire avec une couleur aléatoire
+$("#alea").click(function(){
+	caseAlea = Math.floor((255-0)*Math.random());
+	coulR = Math.floor((255-0)*Math.random());
+	coulG = Math.floor((255-0)*Math.random());
+	coulB = Math.floor((255-0)*Math.random());
+	coulAlea = "rgb("+coulR+","+coulG+","+coulB+")";
+
+	$(".case"+caseAlea).css("background-color", coulAlea);
+	$.ajax({
+			url: 'http://10.200.0.156/js/tutos/jqueryExos/draw.php',
+			type: 'POST',
+			data: {numcase: caseAlea, couleur: coulAlea}
+		});
+})
